2db79824ae9fc742b6367db2b0ede01afcc9d27e
kdrmadusanka <ravmkdon@gmail.com>
2025-03-12 22:42:08 +0530

    Create, get all users functionality.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,13 @@
 import mongoose from 'mongoose';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+
+// Get all users
+export const getUsers = async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+  };
