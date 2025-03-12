fb5a435cb0ef49a632953fcfca5890b50a57bc07
kdrmadusanka <ravmkdon@gmail.com>
2025-03-12 22:43:47 +0530

    Create, update user role (admin only) functionality.

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,3 +25,20 @@ export const getUserById = async (req, res) => {
     }
 };
 
+// Update user role (admin only)
+export const updateUserRole = async (req, res) => {
+    try {
+      const user = await User.updateOne(
+        { id: req.params.id },
+        { $set: { role: req.body.role } }
+      );
+  
+      if (user.modifiedCount === 0) {
+        return res.status(404).json({ message: 'Role already the same' });
+      }
+  
+      res.status(200).json({ message: 'The user role has been updated', user });
+    } catch (error) {
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+};
