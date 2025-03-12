83032b4dc15af6e04775328cbd75a183d74d4683
kdrmadusanka <ravmkdon@gmail.com>
2025-03-12 22:47:43 +0530

    Create delete a user by id functionality.

--- controllers/userController.js
+++ controllers/userController.js
@@ -153,3 +153,26 @@ export const changePassword = async (req, res) => {
       res.status(500).json({ message: 'Something went wrong' });
     }
 };
+
+// Delete a user by id 
+export const deleteUser = async (req, res) => {
+    const userId = req.params.id;
+  
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+      return res.status(400).json({ message: "Invalid user ID" });
+    }
+  
+    try {
+      const userDeletion = await User.deleteOne({_id: userId});
+  
+      if(userDeletion.deletedCount === 0) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      res.status(200).json({ message: "User deleted successfully" });
+  
+    } catch (error) {
+      console.error('Error deleting user', error);
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+};
