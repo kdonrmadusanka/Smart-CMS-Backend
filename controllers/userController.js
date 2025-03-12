0395ea87682c73e4a1d0d07cbdff5b01b0e38b1c
kdrmadusanka <ravmkdon@gmail.com>
2025-03-12 22:43:01 +0530

    Create get a single user by ID functionality.

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,4 +10,18 @@ export const getUsers = async (req, res) => {
     } catch (error) {
       res.status(500).json({ message: 'Something went wrong' });
     }
-  };
+};
+
+// Get a single user by ID
+export const getUserById = async (req, res) => {
+    try {
+      const user = await User.findOne({id: req.params.id});
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+};
+
