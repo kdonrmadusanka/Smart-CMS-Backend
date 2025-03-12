e7926fc2ab5beaba55604b71cfd0625a3e8d6b0a
kdrmadusanka <ravmkdon@gmail.com>
2025-03-12 22:46:41 +0530

    Create, update password functionality.

--- controllers/userController.js
+++ controllers/userController.js
@@ -120,3 +120,36 @@ export const getUserActivityAnalytics = async (req, res) => {
       res.status(500).json({ message: 'Something went wrong' });
     }
 };
+
+// Update password functionality
+export const changePassword = async (req, res) => {
+    const { currentPassword, newPassword } = req.body;
+    const userId = req.user._id;
+    try {
+      const user = await User.findById(userId);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const isPasswordValid = await bcrypt.compare(currentPassword, user.password);
+      if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Current password is incorrect' });
+      }
+  
+      const isPasswordSame = await bcrypt.compare(newPassword, user.password);
+      if (isPasswordSame) {
+        return res.status(400).json({ message: 'Your new is password is same to the current password' });
+      }
+  
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(newPassword, salt);
+  
+      user.password = hashedPassword;
+      await user.save();
+  
+      res.status(200).json({ message: 'Password updated successfully' });
+    } catch (error) {
+      console.error('Error changing password:', error);
+      res.status(500).json({ message: 'Something went wrong' });
+    }
+};
